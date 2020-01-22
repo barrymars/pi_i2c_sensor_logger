ed0dd47a0587116d0819481556b0f7c58eaf9f5c
Barry Tucker <marsbarry@googlemail.com>
2020-01-22 19:44:23 +0000

    adding lux to logger and switching to use gobot

--- main.go
+++ main.go
@@ -4,39 +4,43 @@ import (
   "log"
   "time"
 
-  "github.com/d2r2/go-i2c"
-  "github.com/d2r2/go-bsbmp"
-  "github.com/d2r2/go-logger"
   "github.com/influxdata/influxdb1-client/v2"
+  "gobot.io/x/gobot/drivers/i2c"
+  "gobot.io/x/gobot/platforms/raspi"
 )
 
 func main() {
-  i2c_bme280, err := i2c.NewI2C(0x76, 1)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer i2c_bme280.Close()
-  logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
+  r := raspi.NewAdaptor()
 
-  bme280, err := bsbmp.NewBMP(bsbmp.BME280, i2c_bme280)
+  bme280 := i2c.NewBME280Driver(r, i2c.WithBus(1), i2c.WithAddress(0x76))
+  err := bme280.Start()
   if err != nil {
     log.Fatal(err)
   }
-  logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
+  t, err := bme280.Temperature()
+  if err != nil {
+    log.Fatal(err)
+  }
+  p, err := bme280.Pressure()
+  if err != nil {
+    log.Fatal(err)
+  }
+  h, err := bme280.Humidity()
+  if err != nil {
+    log.Fatal(err)
+  }
+  bme280.Halt()
 
-  t, err := bme280.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
+  tsl2561 := i2c.NewTSL2561Driver(r, i2c.WithBus(1), i2c.WithAddress(0x39))
+  err = tsl2561.Start()
   if err != nil {
     log.Fatal(err)
   }
-  p, err := bme280.ReadPressurePa(bsbmp.ACCURACY_STANDARD)
+  bb, ir, err := tsl2561.GetLuminocity()
   if err != nil {
     log.Fatal(err)
   }
-  p = p / 100
-  _, h, err := bme280.ReadHumidityRH(bsbmp.ACCURACY_STANDARD)
-  if err != nil {
-    log.Fatal(err)
-  }
+  tsl2561.Halt()
 
   c, err := client.NewHTTPClient(client.HTTPConfig{
     Addr: "http://192.168.0.45:8086",
@@ -57,33 +61,33 @@ func main() {
   }
 
   tags := map[string]string{"location":"front_lounge"}
-  fields := map[string]interface{}{
+  tph_fields := map[string]interface{}{
     "temperature": t,
     "pressure": p,
     "humidity": h,
   }
-  tp, err := client.NewPoint("temp_pressure_humidity", tags, fields, time.Now())
+  tph, err := client.NewPoint("temp_pressure_humidity", tags, tph_fields, time.Now())
   if err != nil {
     log.Fatal(err)
   }
-  bp.AddPoint(tp)
+  bp.AddPoint(tph)
+  lux_fields := map[string]interface{}{
+    "broadband": bb,
+    "infrared": ir,
+  }
+  lux, err := client.NewPoint("luminocity", tags, lux_fields, time.Now())
+  if err != nil {
+    log.Fatal(err)
+  }
+  bp.AddPoint(lux)
   err = c.Write(bp)
   if err != nil {
     log.Fatal(err)
   }
+
   log.Printf("Temperature: %v*C", t)
   log.Printf("Pressure: %v hPa", p)
   log.Printf("Humidity: %v %%RH", h)
-
-  i2c_tsl2561, err := i2c.NewI2C(0x39, 1)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer i2c_tsl2561.Close()
-
-  _, err := NewTSL2561()
-  if err != nil {
-    log.Fatal(err)
-  }
-
+  log.Printf("Broadband Luminocity: %v lux", bb)
+  log.Printf("InfraRed Luminocity: %v lux", ir)
 }
